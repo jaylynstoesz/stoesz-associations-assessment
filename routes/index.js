f07792923a1f5dfbf59dbb0abaf42f5fc11b64c6
Jaylyn Stoesz <jaylynstoesz@gmail.com>
2015-08-14 15:26:13 -0600

    diplay meetings on home page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bcrypt = require('bcryptjs');
+var db = require('../lib/mongoose.js');
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/mm');
@@ -17,8 +18,8 @@ var userSchema = new mongoose.Schema({
 
 var meetingSchema = new mongoose.Schema({
   date: Date,
-  mentor: String,
-  student: String,
+  creator: String,
+  invitee: String,
 })
 
 var User = mongoose.model("User", userSchema)
@@ -30,37 +31,46 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/home', function(req, res, next) {
-  User.findOne({_id: req.cookies.id}, function(err, user){
-    var suggestions = [];
-    var type;
-    var connectionIds = user.connections;
-    var connections = [];
-    if (user.type === "mentor") {
-      type = "student"
-    } else {
-      type = "mentor"
+  // db.findAllUsers();
+  var id = req.cookies.id
+  var allUsers;
+  User.find({}).then(function (result) {
+    allUsers = result
+  })
+
+
+
+  User.findOne({_id: id}).then(function (user) {
+    var meetingList = [];
+    var promiseArray = []
+    for (var i = 0; i < user.meetings.length; i++) {
+      var subProm = []
+      user.meetings[i].meetingObj = {}
+      user.meetings[i].meetingObj.date = user.meetings[i].date
+      subProm.push(User.findOne({_id: user.meetings[i].creator}))
+      subProm.push(User.findOne({_id: user.meetings[i].invitee}))
+      promiseArray.push(Promise.all(subProm))
     }
-    User.find({type: type}, function (err, users) {
-      users.forEach(function (record) {
-        suggestions.push(record)
-      })
-      Promise.all(connectionIds.map(function (id) {
-        if (id) {
-          return User.findOne({_id: id}, function (err, record) {
-            // return record;
-            connections.push(record)
-            // console.log("CONN ARRAY:", connections);
-            // return record
-          })
-          // console.log("CONNECTONS:", connections);
+    Promise.all(promiseArray).then(function (data) {
+      user.meetings.forEach(function (meeting, i) {
+
+        if (meeting.creator.toString() === user._id.toString()) {
+          meeting.meetingObj.creator = "you"
+        } else {
+          meeting.meetingObj.creator = data[i][0].name
         }
-        return connections
-      })).then(function () {
-        console.log("CURRENT USER:", user);
-        res.render('home', {name: user.name, suggestions: suggestions, connections: connections});
+
+        if (meeting.invitee.toString() === user._id.toString()) {
+          meeting.meetingObj.invitee = "you"
+        } else {
+          meeting.meetingObj.invitee = data[i][1].name
+        }
+        
       })
+      console.log('meeting data!!!!', user.meetings)
+      res.render('home', {name: user.name, connections: user.connections, meetings: user.meetings, allUsers: allUsers})
     })
-  });
+  })
 });
 
 router.post('/create', function (req, res, next) {
@@ -73,126 +83,111 @@ router.post('/create', function (req, res, next) {
       type: type,
       name: name,
       email: email,
-      password: password,
-      connections: [],
-      meetings: []
+      password: password
     },
     function(err, user) {
       if (err) {
         console.log(err);
       } else {
-        console.log(user);
+        console.log("NEW USER CREATED:", user);
       }
     }
-  );
-  res.redirect('/home');
+  ).then(function (newUser) {
+    res.cookie('name', newUser.name)
+    res.cookie('id', newUser._id)
+    res.redirect('/home');
+  })
 });
 
 router.post('/login', function (req, res, next) {
-  User.findOne({email: req.body.email}, function(err, user){
-    console.log("user:", user);
-    if (err) {
-      console.log(err);
+  User.findOne({email: req.body.email}).then(function(user){
+    console.log(user);
+    if (user === null) {
+      res.render ('index', {msg: "User not found."})
+    } else if (req.body.password === user.password) {
+      res.cookie("id", user._id);
+      res.cookie("name", user.name);
+      res.redirect('/home');
     } else {
-      if (user === null) {
-        res.render ('index', {msg: "User not found."})
-      } else if (req.body.password === user.password) {
-        res.cookie("id", user._id);
-        res.redirect('/home');
-      } else {
-        res.render('index', {msg: "Password does not match."})
-      }
+      res.render('index', {msg: "Password does not match."})
     }
-  });
+  })
 });
 
 router.get('/logout', function (req, res, next) {
   res.clearCookie('id');
-  res.redirect('/');
-});
+  res.clearCookie('name');
+  res.redirect('/')
+})
 
 router.get('/deleteAcct', function (req, res, next) {
-  res.clearCookie('id');
   User.findOne({_id: req.cookies.id}, function (err, record) {
     record.remove();
+    res.clearCookie('id');
+    res.clearCookie('name');
     res.redirect('/');
   });
-});
+})
 
 router.get('/profile/:id', function (req, res, next) {
-  User.findOne({_id: req.params.id}, function (err, personDoc) {
-    console.log("PROFILE FOR:", personDoc);
-    Meeting.find({mentor: req.params.id}, function (err, meetingDocs) {
-      res.render('profile', {thePerson: personDoc, meetings: meetingDocs})
+  var meetings = Meeting.find({creator: req.params.id}, function (err, meetingDocs) {
+    return meetingDocs
+  }).then(function (result) {
+    return Meeting.find({invitee: req.params.id}, function (err, meetingDocs) {
+      return meetingDocs.concat(result)
+    })
+  }).then(function (allMeetings) {
+    User.findOne({_id: req.params.id}).then(function (record) {
+      console.log("PROFILE FOR:", record);
+      return record
+    }).then(function (record) {
+      res.render('profile', {thePerson: record, meetings: allMeetings})
+    })
+  })
+})
+
+router.post('/meeting/:id', function (req, res, next) {
+  var date = req.body.date
+  User.findOne({_id: req.params.id}).then(function (record) {
+    return record
+  }).then(function (record) {
+    var newMeeting;
+    Meeting.create(
+      {
+        date: req.body.date,
+        creator: req.cookies.id,
+        invitee: record._id
+      },
+      function(err, meeting) {
+        if (err) {
+          console.log(err);
+        } else {
+          newMeeting = meeting;
+          console.log(meeting);
+        }
+      }
+    ).then(function () {
+      console.log("RECORD:", record.meetings);
+      var meetTemp = record.meetings;
+      meetTemp.push(newMeeting);
+      console.log("NEW MEETING:", meetTemp);
+      return User.update({_id: record._id}, {$set: {meetings: meetTemp}});
+    }).then(function () {
+      User.findOne({_id: req.cookies.id}).then(function (record) {
+        var meetTemp = record.meetings;
+        meetTemp.push(newMeeting);
+        return User.update({_id: record._id}, {$set: {meetings: meetTemp}});
+      })
     })
   });
-});
-
-router.get('/meeting/:id', function (req, res, next) {
-  var date = new Date();
-  var mentor = req.params.id;
-  var student = req.cookies.id;
-  Meeting.create(
-    {
-      date: date,
-      mentor: mentor,
-      student: student
-    },
-    function(err, meeting) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(meeting);
-      }
-    }
-  );
   res.redirect('/profile/' + req.params.id)
-});
+})
 
 router.get('/connect/:id', function (req, res, next) {
-  User.findOne({_id: req.cookies.id}).then(function (record) {
-    var connections = record.connections.push(req.params.id);
-    return record.connections
-  }).then(function (result) {
-    return User.update({_id: req.cookies.id}, {$set: {connections: result}})
-  }).then(function () {
-    User.findOne({_id: req.params.id}).then(function (record) {
-      var connections = record.connections.push(req.cookies.id);
-      return record.connections
-    }).then(function (result) {
-      return User.update({_id: req.params.id}, {$set: {connections: result}})
-    })
-  }).then(function () {
-    res.redirect('/home');
-  })
+
 })
 
 router.get('/disconnect/:id', function (req, res, next) {
-  User.findOne({_id: req.cookies.id}).then(function (record) {
-    var connections = record.connections;
-    var connectionsTemp = connections.map(function (id) {
-      if (id !== req.params.id) {
-        return id;
-      }
-    })
-    return connectionsTemp;
-  }).then(function (result) {
-    return User.update({_id: req.cookies.id}, {$set: {connections: result}})
-  }).then(function () {
-    User.findOne({_id: req.params.id}).then(function (record) {
-      var connections = record.connections;
-      var connectionsTemp = connections.map(function (id) {
-        if (id !== req.cookies.id) {
-          return id;
-        }
-      })
-      return connectionsTemp;
-    }).then(function (result) {
-      return User.update({_id: req.params.id}, {$set: {connections: result}})
-    })
-  }).then(function () {
-    res.redirect('/home')
-  })
-})
 
+})
 module.exports = router;
